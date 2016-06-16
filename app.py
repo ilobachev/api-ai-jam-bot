bc3bd9832b92fba43a235f0547ed2a02ffb0a698
Ivan Lobachev <ilobachev@speaktoit.com>
2016-06-16 16:40:09 -0700

    Simple webhook for API.AI bot.

---
+++ app.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+data = {}
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "jam.location.get":
+        return {}
+
+    location = req.get("result").get("parameters").get("Location")
+
+    data.setdefault('locations', []).append(location)
+
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeWebhookResult(data):
+    return data
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": ''.join(data['locations']),
+        "displayText": ''.join(data['locations']),
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=False, port=port, host='0.0.0.0')
