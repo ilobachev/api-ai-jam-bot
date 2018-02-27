31d4de317f09ddfd6245442dea025f193aff32cf
ilobachev <ilobachev@gmail.com>
2018-02-26 17:16:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -29,34 +29,67 @@ def webhook():
     print(r)
     return r
 
-
 def processRequest(req):
-    if req.get("result").get("action") != 'jam.location.get':
+    if req.get("result").get("action") != "weather":
         return {}
-
-    print(req.get("result").get("parameters"))
-    #location = req.get("result").get("parameters").get("Location")
-    #location = []
-
-    #data.setdefault('locations', []).append(location)
-
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
     res = makeWebhookResult(data)
     return res
 
 
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("address")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
 def makeWebhookResult(data):
-#    return data
-    #locations = ''.join(data['locations'])
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
     print("Response:")
+    print(speech)
 
     return {
-        "speech": "response",
-        "displayText": "response",
+        "speech": speech,
+        "displayText": speech,
         # "data": data,
-        "contextOut": [{"name":"sendinvoicebyemail","lifespan":0},{"name":"prompt","lifespan":5}],
+        # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
-
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
