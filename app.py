fe13594218041c12f38bb636cd14377544297169
ilobachev <ilobachev@gmail.com>
2018-02-26 18:35:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
-import urllib
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
 import os
 
@@ -10,8 +17,6 @@ from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
-
-data = {}
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
